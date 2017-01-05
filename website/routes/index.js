2c3879d6b9eefce0b08c6598db8749b2566b0b38
qianxuehuanyu <dylizhenyu@163.com>
2017-01-05 17:30:31 +0800

    index首页接口修改

--- website/routes/index.js
+++ website/routes/index.js
@@ -5,13 +5,44 @@ const conn = require("./MySqlConnection");
 /* GET home page. */
 //index
 router.get('/index.html', function(req, res, next) {
+	var indexContent={
+		tilte:"亿街区官方网站",
+		navUrl:{},
+		asideUrl:{},
+		footerUrl:{},
+		indexUrl:{},
+		indexHzhb:{},
+		indexYxal:{}
+	};
     conn.query("SELECT * FROM nav_url", function (err, result) {
       if (!err) {
-      	res.render("./index", {title:"亿街区官方网站",navUrl: result});
-      } else {
-        res.json(err);
-      }
-    });
+      	indexContent.navUrl=result;
+      }else{console.log(err)}});
+    conn.query("SELECT * FROM aside_url", function (err, result) {
+      if (!err) {
+      	indexContent.asideUrl=result;
+      }else{console.log(err)}});
+    conn.query("SELECT * FROM footer_url", function (err, result) {
+      if (!err) {
+      	indexContent.footerUrl=result;
+      }else{console.log(err)}});
+	conn.query("SELECT * FROM index_url", function (err, result) {
+      if (!err) {
+      	indexContent.indexUrl=result;
+      }else{console.log(err)}});
+	conn.query("SELECT * FROM index_hzhb", function (err, result) {
+      if (!err) {
+      	indexContent.indexHzhb=result;
+      }else{console.log(err)}});
+	conn.query("SELECT * FROM index_yxal", function (err, result) {
+      if (!err) {
+      	indexContent.indexYxal=result;
+      }else{console.log(err)}});
+	conn.query("SELECT * FROM index_hzhb", function (err, result) {
+      if (!err) {
+      	indexContent.indexHzhb=result;
+      }else{console.log(err)}});
+	res.render("./index",indexContent);
 });
 
 module.exports = router;
