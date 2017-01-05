f32b272e5ab9aaddcbf6de7387dd55d7dcb2e9b2
Vera <verahuang1994@hotmail.com>
2017-01-05 09:05:58 +0800

    ptjs.js和ptjs.html的更新

--- website/public/javascripts/ptjs/ptjs.js
+++ website/public/javascripts/ptjs/ptjs.js
@@ -1,3 +1,11 @@
-(function(){
 
+(function(){
+	var list = $(".style_img3 span");
+	console.log(list);
+
+	list.click(function(event) {
+		/* Act on the event */
+		list.removeClass('now');
+		$(this).addClass('now');
+	});
 })();
