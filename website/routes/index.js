df0512bcd60aa66aba0c0790a90249c8390f1ffd
qianxuehuanyu <dylizhenyu@163.com>
2017-01-06 11:15:25 +0800

    index异步定时器添加BY厉震宇

--- website/routes/index.js
+++ website/routes/index.js
@@ -7,6 +7,7 @@ const conn = require("./MySqlConnection");
 router.get('/index.html', function(req, res, next) {
 	var indexcontent={
 		title:"亿街区官方网站",
+    select:0,
 		navUrl:{},
 		asideUrl:{},
 		footerUrl:{},
@@ -17,28 +18,45 @@ router.get('/index.html', function(req, res, next) {
     conn.query("SELECT * FROM nav_url", function (err, result) {
       if (!err) {
       	indexcontent.navUrl=result;
-      }else{console.log(err)}});
+      }else{console.log(err)}
+      indexcontent.select++;
+    });
     conn.query("SELECT * FROM aside_url", function (err, result) {
       if (!err) {
       	indexcontent.asideUrl=result;
-      }else{console.log(err)}});
+      }else{console.log(err)}
+    indexcontent.select++;
+    });
     conn.query("SELECT * FROM footer_url", function (err, result) {
       if (!err) {
       	indexcontent.footerUrl=result;
-      }else{console.log(err)}});
+      }else{console.log(err)}
+      indexcontent.select++;
+    });
 	conn.query("SELECT * FROM index_url", function (err, result) {
       if (!err) {
       	indexcontent.indexUrl=result;
-      }else{console.log(err)}});
+      }else{console.log(err)}
+      indexcontent.select++;
+    });
 	conn.query("SELECT * FROM upload_hzhb", function (err, result) {
       if (!err) {
       	indexcontent.uploadHzhb=result;
-      }else{console.log(err)}});
+      }else{console.log(err)}
+      indexcontent.select++;
+    });
 	conn.query("SELECT * FROM upload_yxal", function (err, result) {
       if (!err) {
       	indexcontent.uploadYxal=result;
-      }else{console.log(err)}});
-	res.render("./index",{indexContent:indexcontent});
+      }else{console.log(err)}
+      indexcontent.select++;
+    });
+    var sqlselect=setInterval(function(){
+      if(indexcontent.select==6){
+        clearInterval(sqlselect);
+        res.render("./index",{indexContent:indexcontent});
+      }
+    },100);
 });
 
 module.exports = router;
