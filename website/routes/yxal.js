3dfc6add6cfe6932045b1feda1ff85a0881a9fbf
qianxuehuanyu <dylizhenyu@163.com>
2017-01-07 14:40:44 +0800

    yxal.js修改BY：厉震宇

--- website/routes/yxal.js
+++ website/routes/yxal.js
@@ -44,7 +44,49 @@ router.get('/yxal.html', function(req, res, next) {
       }
     },100);
 });
+///////////////////////////////////
+///////////////////////////////////
+//yxal10.html
+router.get('/yxal10.html', function(req, res, next) {
+  console.log(req.body.id);
+  var yxalcontent={
+    title:"亿街区官方网站",
+    navUrl:{},
+    navTitle:"yxal",
+    footerUrl:{},
+    selectCount:0,
+    uploadYxal:{}
+  };
+  var connquery_count=0;
+    conn.query("SELECT * FROM nav_url", function (err, result) {
+      if (!err) {
+        yxalcontent.navUrl=result;
+        yxalcontent.selectCount++;
+      }else{console.log(err)}
+      connquery_count++;
+    });
+    conn.query("SELECT * FROM footer_url", function (err, result) {
+      if (!err) {
+        yxalcontent.footerUrl=result;
+        yxalcontent.selectCount++;
+      }else{console.log(err)}
+      connquery_count++;
+    });
 
+    conn.query("SELECT * FROM upload_yxal", function (err, result) {
+    if (!err) {
+      yxalcontent.uploadYxal=result;
+      yxalcontent.selectCount++;
+    } else {console.log(err);}
+    connquery_count++;
+  });
+  var sqlselect=setInterval(function(){
+      if(connquery_count==3){
+        clearInterval(sqlselect);
+        res.render("./yxal/yxal", {Content: yxalcontent});
+      }
+    },100);
+});
 
 
 module.exports = router;
