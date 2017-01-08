10f7971a67ab94e7282741a1b41068f41c2b3363
qianxuehuanyu <dylizhenyu@163.com>
2017-01-08 17:17:30 +0800

    后端修改BY：厉震宇

--- website/routes/jmhz.js
+++ website/routes/jmhz.js
@@ -4,16 +4,16 @@ var router = express.Router();
 
 const conn = require("./MySqlConnection");
 /* GET home page. */
-//yjqxw
-router.get('/yjqxw.html', function(req, res, next) {
+//jmhz
+router.get('/jmhz.html', function(req, res, next) {
    
-	var yjqxwcontent={
+	var jmhzwcontent={
 		title:"亿街区官方网站",
 		navUrl:{},
-		navTitle:"yjqxw",
+		navTitle:"jmhz",
 		footerUrl:{},
-  		selectCount:0,
-  		kindeList:{},
+  	selectCount:0,
+  	kindeList:{},
 		uploadYjqxw:{}
 	};
 	var connquery_count=0;
@@ -48,54 +48,13 @@ router.get('/yjqxw.html', function(req, res, next) {
   var sqlselect=setInterval(function(){
       if(connquery_count==4){
         clearInterval(sqlselect);
-        res.render("./yjqxw/yjqxw", {Content: yjqxwcontent});
+        res.render("./hzhb/hzhb", {Content: yjqxwcontent});
       }
     },100);
 });
 
 ///////////////////////////////////////
 ///////////////////////////////////////
-//yjqxw2
 
-router.get('/yjqxw2.html', function(req, res, next) {
-	console.log(req.body.id);
-   
-	var yjqxwcontent={
-		title:"亿街区官方网站",
-		navUrl:{},
-		navTitle:"yjqxw",
-		footerUrl:{},
-  		selectCount:0,
-		uploadYjqxw:{}
-	};
-	var connquery_count=0;
-  	conn.query("SELECT * FROM nav_url", function (err, result) {
-      if (!err) {
-      	yjqxwcontent.navUrl=result;
-        yjqxwcontent.selectCount++;
-      }else{console.log(err)}
-      connquery_count++;
-    });
-    conn.query("SELECT * FROM footer_url", function (err, result) {
-      if (!err) {
-      	yjqxwcontent.footerUrl=result;
-        yjqxwcontent.selectCount++;
-      }else{console.log(err)}
-      connquery_count++;
-    });
-  	conn.query("SELECT * FROM `upload_yjqxw` WHERE `url_id`="+req.body.id, function (err, result) {
-    if (!err) {
-    	yjqxwcontent.uploadYjqxw=result;
-    	yjqxwcontent.selectCount++;
-    } else {console.log(err);}
-    connquery_count++;
-  });
-  var sqlselect=setInterval(function(){
-      if(connquery_count==3){
-        clearInterval(sqlselect);
-        res.render("./yjqxw/yjqxw2", {Content: yjqxwcontent});
-      }
-    },100);
-});
 
 module.exports = router;
