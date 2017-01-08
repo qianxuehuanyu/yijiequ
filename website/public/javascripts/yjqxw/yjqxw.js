3f2ff2aba775427f83597b1effbe70333eceea08
daughlas <daughlas@gmail.com>
2017-01-08 23:13:27 +0800

    finish 20170708 work

--- website/public/javascripts/yjqxw/yjqxw.js
+++ website/public/javascripts/yjqxw/yjqxw.js
@@ -1,47 +1,14 @@
 (function() {
 
-	var jsonData = {};
-	var classes = [];
-	var articles = [];
+	console.log(1);
+	// var articleContainer = document.getElementById("yjqxw-article");
 
-	$.ajax({
-		type: 'GET',
-		url: "/data/yjqxw/yjqxw.json",
-		dataType: "json",
-		success: function(file) {
-			jsonData = file;
-			classes = file.classes;
-			articles = file.articles;
-			render();
-		},
-		error: function() {
-			alert(error);
-		}
-	});
+	// console.log(articleContainer);
+	// var text = articleContainer.textContent;
+	// articleContainer.innerHtml = text;
 
-	function renderCategories() {
-		var eleNewsCategoryUl = $(".news-category");
-		var eleNewsCategoryLi = $(".news-category-item");
 
-		var categoriesHtml = "";
-		for(var i=0; i<classes.length; i++) {
-			categoriesHtml += "<li class='news-category-item'><a href='#'>"+classes[i]+"</a></li>";
-			// var x = classes.length;
-			// var liWidth = (1/x)/%
-			eleNewsCategoryLi.css("width","50px");
 
-		}
-		// var tempHtml = eleNewsCategory.html() + categoriesHtml;
-		eleNewsCategory.html(eleNewsCategoryUl.html() + categoriesHtml);
-	}
 
-	function renderArticles() {
-
-	}
-
-	function render() {
-		renderCategories();
-		renderArticles();
-	}
 
 })();
