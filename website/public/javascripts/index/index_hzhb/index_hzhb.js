841e2fa8db76bdce959191231220ffb8d1b895b8
孙佳俊 <429832306@qq.com>
2017-01-10 09:27:52 +0800

    index_hzhb  js完成

--- website/public/javascripts/index/index_hzhb/index_hzhb.js
+++ website/public/javascripts/index/index_hzhb/index_hzhb.js
@@ -10,23 +10,24 @@ $(document).ready(function () {
     var hz=$(".hezuo");
     var wy=$(".wuye");
 
-        sysj[0].click(function (event) {
-            sy[0].show();
-            hz[0].hide();
-            wy[0].hide();
-            console.log(11)
-        });
-    hzsj[0].click(function (event) {
-        hz[0].show();
-        sy[0].hide();
-        wy[0].hide();
+    console.log(hzsj);
+        sysj[0].onclick=function () {
+            sy[0].style.display="block";
+            hz[0].style.display="none";
+            wy[0].style.display="none";
+            console.log(sy)
+        }
+    hzsj[0].onclick=function (event) {
+        sy[0].style.display="none";
+        hz[0].style.display="block";
+        wy[0].style.display="none";
         console.log(22)
-    });
+    };
 
-    wyqy[0].click(function (event) {
-        wy[0].show();
-        hz[0].hide();
-        sy[0].hide();
+    wyqy[0].onclick=function (event) {
+        sy[0].style.display="none";
+        hz[0].style.display="none";
+        wy[0].style.display="block";
         console.log(33)
-    });
+    };
 });
