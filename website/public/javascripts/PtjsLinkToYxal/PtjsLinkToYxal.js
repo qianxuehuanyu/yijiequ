5a712519a190dbb487e0fb4f02edc7aac53def3e
Vera <verahuang1994@hotmail.com>
2017-01-11 15:59:34 +0800

    PtjsLinkToYxal.ejs修改

--- website/public/javascripts/PtjsLinkToYxal/PtjsLinkToYxal.js
+++ website/public/javascripts/PtjsLinkToYxal/PtjsLinkToYxal.js
@@ -1,19 +1,15 @@
 (function(){
 	var list = $(".siteList span");
-	var nowSpan = $(".siteList .now_span");
+	// var nowSpan = $(".siteList .now_span");
 	console.log(list);
 
-	for (var i = 0; i < list.length; i++) {
-		console.log(typeof list[i].innerHTML);
-		console.log(typeof nowSpan.val());
-		if (list[i].innerHTML==nowSpan.val()) {
-			// list[i].addClass('now');
-			console.log(1111);
-		}else{
-			console.log(2222);
-			// console.log(list[i]);
-		}
-	}
+	// for (var i = 0; i < list.length; i++) {
+	// 	if (String(list[i].innerHTML)==String(nowSpan.val())) {
+	// 		console.log(1111);
+	// 	}else{
+	// 		console.log(2222);
+	// 	}
+	// }
 
 	list.click(function(event) {
 		/* Act on the event */
